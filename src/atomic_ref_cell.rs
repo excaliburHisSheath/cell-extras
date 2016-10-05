ca9c3a7216d9c6e92405a656245324a0e730cfe2
David LeGare <david.legare@scientificgames.com>
2016-10-05 13:22:36 -0500

    Add documentation for AtomicRefCell

--- src/atomic_ref_cell.rs
+++ src/atomic_ref_cell.rs
@@ -1,4 +1,5 @@
 use std::cell::UnsafeCell;
+use std::fmt::{self, Debug, Formatter};
 use std::ops::{Deref, DerefMut};
 use std::sync::atomic::{AtomicUsize, Ordering};
 
@@ -15,7 +16,23 @@ const WRITING: usize = !0;
 /// access it mutably at once. Instead it will panic, which will help track down any bugs that
 /// would cause that to happen.
 ///
+/// The `borrow()` and `borrow_mut()` methods allow you to create a runtime borrow of the cell's
+/// data. `borrow()` returns an `AtomicRef<T>` which can be treated as a `&T`, and `borrow_mut()`
+/// returns an `AtomicRefMut<T>` which can be treated as a `&mut T`. The borrow rules for
+/// `AtomicRefCell` are the same as the rules enforced by the compiler: You may have either (0 or
+/// more immutable borrows) OR (0 or 1 mutable borrow). A call to `borrow()` while there
+/// is an active mutable borrow, or a call to `borrow_mut()` while there is an active immutable
+/// borrow, will result in a panic. If panicking is not desirable, `try_borrow()` and
+/// `try_borrow_mut()` return an `Option<AtomicRef<T>>` and an `Option<AtomicRefMut<T>>`,
+/// respectively, both returning `None` if the borrow is not possible at that time.
+///
+/// [refcell]: https://doc.rust-lang.org/std/cell/struct.RefCell.html
+/// [mutex]: https://doc.rust-lang.org/std/sync/struct.Mutex.html
+/// [rwlock]: https://doc.rust-lang.org/std/sync/struct.RwLock.html
+///
 /// # Examples
+///
+/// Basic usage:
 ///
 /// ```
 /// use cell_extras::AtomicRefCell;
@@ -42,6 +59,15 @@ pub struct AtomicRefCell<T> {
 }
 
 impl<T> AtomicRefCell<T> where T: Send + Sync {
+    /// Create a new `AtomicRefCell` containing `value`.
+    ///
+    /// # Examples
+    ///
+    /// ```
+    /// use cell_extras::AtomicRefCell;
+    ///
+    /// let cell = AtomicRefCell::new(5);
+    /// ```
     pub fn new(value: T) -> AtomicRefCell<T> {
         AtomicRefCell {
             borrow: AtomicUsize::new(UNUSED),
@@ -49,15 +75,106 @@ impl<T> AtomicRefCell<T> where T: Send + Sync {
         }
     }
 
+    /// Consumes the `AtomicRefCell`, returning the wrapped value.
+    ///
+    /// This is always safe to do because you must consume the `AtomicRefCell`, which cannot happen
+    /// if there are any active borrows of its value.
+    ///
+    /// # Examples
+    ///
+    /// ```
+    /// use cell_extras::AtomicRefCell;
+    ///
+    /// let cell = AtomicRefCell::new(5);
+    ///
+    /// let inner = cell.into_inner();
+    /// assert_eq!(5, inner);
+    /// ```
     pub fn into_inner(self) -> T {
         debug_assert!(self.borrow.load(Ordering::SeqCst) == UNUSED);
         unsafe { self.value.into_inner() }
     }
 
+    /// Immutably borrow the wrapped value.
+    ///
+    /// The borrow lasts until the returned `AtomicRef` exits scope or is otherwise dropped.
+    /// Leaking the returned `AtomicRef` will result in the borrow never ending, so don't do that.
+    ///
+    /// # Panics
+    ///
+    /// - If the value is currently mutably borrowed. For a non-panicking variant, use `try_borrow()`.
+    ///
+    /// # Examples
+    ///
+    /// Borrow the cell multiple times across multiple threads:
+    ///
+    /// ```
+    /// use cell_extras::AtomicRefCell;
+    /// use std::sync::Arc;
+    /// use std::thread;
+    ///
+    /// let cell = Arc::new(AtomicRefCell::new(7));
+    ///
+    /// let borrow0 = cell.borrow();
+    ///
+    /// let clone = cell.clone();
+    /// thread::spawn(move || {
+    ///     let borrow1 = clone.borrow();
+    ///     let borrow2 = clone.borrow();
+    ///
+    ///     // `borrow1` and `borrow2` end here.
+    /// }).join().unwrap();
+    ///
+    /// let borrow1 = cell.borrow();
+    /// let borrow2 = cell.borrow();
+    /// ```
+    ///
+    /// An example of panic:
+    ///
+    /// ```
+    /// use cell_extras::AtomicRefCell;
+    /// use std::sync::Arc;
+    /// use std::thread;
+    ///
+    /// let cell = Arc::new(AtomicRefCell::new(7));
+    ///
+    /// let borrow0 = cell.borrow_mut();
+    ///
+    /// let clone = cell.clone();
+    /// let result = thread::spawn(move || {
+    ///     let borrow1 = clone.borrow(); // This causes a panic.
+    /// }).join();
+    ///
+    /// assert!(result.is_err());
+    /// ```
     pub fn borrow(&self) -> AtomicRef<T> {
         self.try_borrow().expect("Already mutably borrowed")
     }
 
+    /// Immutably borrow the wrapped value if it's not currently borrowed mutably.
+    ///
+    /// The borrow lasts until the returned `AtomicRef` exits scope or is otherwise dropped.
+    /// Leaking the returned `AtomicRef` will result in the borrow never ending, so don't do that.
+    ///
+    /// This is the non-panicking version of `borrow()`.
+    ///
+    /// # Examples
+    ///
+    /// ```
+    /// use cell_extras::AtomicRefCell;
+    ///
+    /// let cell = AtomicRefCell::new(5);
+    ///
+    /// {
+    ///     let borrow = cell.borrow();
+    ///     assert!(cell.try_borrow().is_some());
+    /// }
+    ///
+    /// {
+    ///     let mut_borrow = cell.borrow_mut();
+    ///     assert!(cell.try_borrow().is_none());
+    /// }
+    /// ```
     pub fn try_borrow(&self) -> Option<AtomicRef<T>> {
         // NOTE: We can't just do `self.borrow.fetch_add(1) != WRITING` because `WRITING` is
         // `usize::MAX`, and adding 1 to it would overflow the value to `UNUSED`, potentially
@@ -74,6 +191,92 @@ impl<T> AtomicRefCell<T> where T: Send + Sync {
         }
     }
 
+    /// Mutably borrow the wrapped value.
+    ///
+    /// The borrow lasts until the returned `AtomicRefMut` exits scope or is otherwise dropped.
+    /// Leaking the returned `AtomicRefMut` will result in the borrow never ending. This is even
+    /// worse than leaking an `AtomicRef` because it will no longer be possible to borrow the
+    /// contents mutable or immutably, the `AtomicRefCell` will efectively become fused. So don't
+    /// do it.
+    ///
+    /// # Panics
+    ///
+    /// - If the value is currently immutably borrowed. For a non-panicking variant, use `try_borrow_mut()`.
+    ///
+    /// # Examples
+    ///
+    /// Borrow the cell across multiple threads:
+    ///
+    /// ```
+    /// use cell_extras::AtomicRefCell;
+    /// use std::sync::Arc;
+    /// use std::thread;
+    ///
+    /// let cell = Arc::new(AtomicRefCell::new(7));
+    ///
+    /// let clone = cell.clone();
+    /// thread::spawn(move || {
+    ///     let mut borrow1 = clone.borrow_mut();
+    ///     *borrow1 += 12;
+    ///
+    ///     // `borrow1` ends here.
+    /// }).join().unwrap();
+    ///
+    /// let borrow = cell.borrow();
+    /// assert_eq!(19, *borrow);
+    /// ```
+    ///
+    /// An example of panic:
+    ///
+    /// ```
+    /// use cell_extras::AtomicRefCell;
+    /// use std::sync::Arc;
+    /// use std::thread;
+    ///
+    /// let cell = Arc::new(AtomicRefCell::new(7));
+    ///
+    /// let borrow0 = cell.borrow();
+    ///
+    /// let clone = cell.clone();
+    /// let result = thread::spawn(move || {
+    ///     let borrow1 = clone.borrow_mut(); // This causes a panic.
+    /// }).join();
+    ///
+    /// assert!(result.is_err());
+    /// ```
+    pub fn borrow_mut(&self) -> AtomicRefMut<T> {
+        self.try_borrow_mut().expect("Already immutably borrowed")
+    }
+
+    /// Mutably borrow the wrapped value if it's not currently borrowed.
+    ///
+    /// The borrow lasts until the returned `AtomicRefMut` exits scope or is otherwise dropped.
+    /// Leaking the returned `AtomicRefMut` will result in the borrow never ending. This is even
+    /// worse than leaking an `AtomicRef` because it will no longer be possible to borrow the
+    /// contents mutable or immutably, the `AtomicRefCell` will efectively become fused. So don't
+    /// do it.
+    ///
+    /// This is the non-panicking version of `borrow_mut()`.
+    ///
+    /// # Examples
+    ///
+    /// ```
+    /// use cell_extras::AtomicRefCell;
+    ///
+    /// let cell = AtomicRefCell::new(5);
+    ///
+    /// assert!(cell.try_borrow_mut().is_some());
+    ///
+    /// {
+    ///     let borrow = cell.borrow();
+    ///     assert!(cell.try_borrow_mut().is_none());
+    /// }
+    ///
+    /// {
+    ///     let mut_borrow = cell.borrow_mut();
+    ///     assert!(cell.try_borrow().is_none());
+    /// }
+    /// ```
     pub fn try_borrow_mut(&self) -> Option<AtomicRefMut<T>> {
         if self.borrow.compare_and_swap(UNUSED, WRITING, Ordering::SeqCst) == UNUSED {
             return Some(AtomicRefMut(self))
@@ -81,9 +284,15 @@ impl<T> AtomicRefCell<T> where T: Send + Sync {
             return None
         }
     }
+}
 
-    pub fn borrow_mut(&self) -> AtomicRefMut<T> {
-        self.try_borrow_mut().expect("Already immutably borrowed")
+impl<T> Debug for AtomicRefCell<T> where T: Debug + Send + Sync {
+    fn fmt(&self, formatter: &mut Formatter) -> Result<(), fmt::Error> {
+        if let Some(value) = self.try_borrow() {
+            write!(formatter, "AtomicRefCell {{ value: {:?} }}", value)
+        } else {
+            write!(formatter, "AtomicRefCell {{ value: <borrowed> }}")
+        }
     }
 }
 
@@ -107,6 +316,12 @@ impl<'a, T: 'a> Deref for AtomicRef<'a, T> {
     }
 }
 
+impl<'a, T: 'a> Debug for AtomicRef<'a, T> where T: Debug {
+    fn fmt(&self, formatter: &mut Formatter) -> Result<(), fmt::Error> {
+        (&**self).fmt(formatter)
+    }
+}
+
 pub struct AtomicRefMut<'a, T: 'a>(&'a AtomicRefCell<T>);
 
 impl<'a, T: 'a> Drop for AtomicRefMut<'a, T> {
@@ -127,5 +342,11 @@ impl<'a, T: 'a> Deref for AtomicRefMut<'a, T> {
 impl<'a, T: 'a> DerefMut for AtomicRefMut<'a, T> {
     fn deref_mut(&mut self) -> &mut T {
         unsafe { &mut *self.0.value.get() }
+    }
+}
+
+impl<'a, T: 'a> Debug for AtomicRefMut<'a, T> where T: Debug {
+    fn fmt(&self, formatter: &mut Formatter) -> Result<(), fmt::Error> {
+        (&**self).fmt(formatter)
     }
 }
